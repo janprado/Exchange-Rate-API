e9578fb91e4fa7959e1b05eaa28eb71f0e017595
Jan Prado <96355787+janprado@users.noreply.github.com>
2022-04-30 15:34:36 +0100

    Exchange Rate API start

---
+++ src/main/java/com/janprado/exchangerateapi/ExchangeRateApiApplication.java
@@ -0,0 +1,14 @@
+package com.janprado.exchangerateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+ExchangeRateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateApiApplication.class, args);
+	}
+
+}
